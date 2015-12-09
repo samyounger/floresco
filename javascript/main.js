a3d0ce1839b888352193ef57be020241bc5172bc
Sam Younger <sam.younger@gmail.com>
2015-12-09 19:36:46 +0000

    Formatting changes, & adding forms
    added slate-background. Added forms to the profile section, and
    textarea boxes to other sections.

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,18 @@
-var data = [
-    [1, 130], [2, 40], [3, 80], [4, 160], [5, 159], [6, 370],
-    [7, 330], [8, 350], [9, 370], [10, 400], [11, 330], [12, 350]
-];
+ 
+    var data = [[1, 130], [2, 40], [3, 80], [4, 160], [5, 159], [6, 370], [7, 330], [8, 350], [9, 370], [10, 400], [11, 330], [12, 350]];
 
-var dataset = [
-    {
-        label: "line1",
-        data: data
-    }
-];
+    var dataset = [{label: "line1",data: data}];
+
+    var options = {
+        series: {
+            lines: { show: true },
+            points: {
+                radius: 1,
+                show: true
+            }
+        }
+    };
+
+    $(document).ready(function () {
+        $.plot($(".chart-placeholder"), dataset, options);
+    });
